3789db19ba3dcd5d21d494fcd754996c20d20867
Leonardo Booz <leonardobooz345@gmail.com>
2026-03-16 22:42:18 -0300

    conclusão da parte 01

---
+++ mavenproject1/src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+
+
+import javax.swing.SwingUtilities;
+import view.Editor;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Leonardo Booz
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            Editor tela = new Editor();
+            tela.setVisible(true);
+        });
+
+    }
+    
+}
